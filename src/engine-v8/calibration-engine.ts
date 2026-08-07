9d7a840551f0878813509f02d75e40c81ea9bf29
Neutral Working <13trianglez@gmail.com>
2026-08-07 22:13:55 +0200

    Keep decay as explicit V8 calibration extension

--- src/engine-v8/calibration-engine.ts
+++ src/engine-v8/calibration-engine.ts
@@ -1,49 +1,9 @@
-export {
-  opposingDepthZone,
-  calibrationRuntimeId,
-  createV8CalibrationState,
-  createV8CalibrationMatch,
-  calibrationPlayersInZone,
-  calibrationPlayerCard,
-  isCalibrationActionEnabled,
-  currentCalibrationAttack,
-  currentCalibrationDefence,
-  hasReducedDefence,
-  calibrationZoneTotals,
-  calibrationTeamTotals,
-  refreshCalibrationSuppression,
-  addCalibrationTacticalToHand,
-  seedCalibrationPlayer,
-  moveCalibrationPlayer,
-  previewCalibrationTacticalCost,
-  playCalibrationTactical,
-  calibrationZoneWinner,
-  calibrationHandTacticals,
-  calibrationHandPlayers,
-  removeCalibrationPlayerFromHand,
-  spendCalibrationTacticalFromHand,
-  resolveCommittedCalibrationTactical,
-  tacticalDefinition,
-} from './calibration-runtime';
-
-export type {
-  V8CalibrationSide,
-  V8ModifierLifetime,
-  V8CalibrationStatModifier,
-  V8CalibrationRuntimePlayer,
-  V8CalibrationHandCard,
-  V8CalibrationTeamState,
-  V8CalibrationOffsideTrap,
-  V8CalibrationTacticalResolution,
-  V8CalibrationEventType,
-  V8CalibrationEvent,
-  V8CalibrationState,
-} from './calibration-runtime';
+export * from './calibration-runtime';
 
 export {
-  applyCalibrationModifier,
-  revealCalibrationPlayer,
-  endV8CalibrationPeriod,
+  applyCalibrationModifier as applyCalibrationDecayModifier,
+  revealCalibrationPlayer as revealCalibrationPlayerWithDecay,
+  endV8CalibrationPeriod as endV8CalibrationPeriodWithDecay,
   calibrationActionText,
   calibrationModifierBadges,
 } from './calibration-decay';
